12e8c07a4afaac3c0410c088cac163e4f6724a2a
kartik <kabakshi@hotmail.ca>
2022-01-15 22:14:03 -0500

    fixing install

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import bodyParser from 'body-parser';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
