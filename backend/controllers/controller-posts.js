1c6de37753fc231f0ca0151182abcb0669a61328
rom <mozzieisme@gmail.com>
2022-01-16 16:48:54 -0500

    Modify the files

--- backend/controllers/controller-posts.js
+++ backend/controllers/controller-posts.js
@@ -1,9 +1,23 @@
+import PostMessage from "../models/postInfo.js"
+
 /*
 this is used to create handlers for our routes so we can call 
 them in the routes / post.js instead of having all that code in one file,
 helps keep code clean
 */
 
-export const getPosts = (req, res) => {
-  res.send("THIS WORKS!")
+export const getPosts = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find()
+
+    res.status(200).json(postMessage)
+
+  } catch (error) {
+    res.status(404).json({message: error.message})
+    
+  }
+}
+
+export const createPost = (req, res) => {
+  res.send("Post Creation")
 }
