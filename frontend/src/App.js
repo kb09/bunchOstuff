ffb10f2ac1eb8151bb05d0fe427474b648a5b02d
Victor Bondarenko <nirasimha@gmail.com>
2022-01-16 17:43:45 -0500

    add responsive layout grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,18 @@ const App = () => {
        <Typography variant="h2" align="center">DejaView</Typography>
        <img src={dejaview} alt="dejaview" height="60" />
      </AppBar>
+     <Grow in>
+       <Container>
+         <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={7}>
+
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            
+          </Grid>
+         </Grid>
+       </Container>
+     </Grow>
    </Container>
   );
 }
