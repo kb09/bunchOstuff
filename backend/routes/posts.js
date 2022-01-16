4c30d7b9f43587429336ba8edfe4f0cfc45a0644
Victor Bondarenko <nirasimha@gmail.com>
2022-01-16 11:09:41 -0500

    handle GET requests to the homepage

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,4 +3,10 @@ import express from 'express';
 //create a new router object
 const router = express.Router();
 
+//handle GET requests to the homepage
+router.get('/', (req, res) => {
+  res.send("This works");
+});
+
+
 export default router
