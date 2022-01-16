56489c98f54433dfbb161b5f412ea5e5e364da2d
kartik <kabakshi@hotmail.ca>
2022-01-16 00:33:57 -0500

    adding and fixing .env information

--- backend/backend-index.js
+++ backend/backend-index.js
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 // import bodyParser from "body-parser";
 
 const app = express(); //set app
+dotenv.config();
 
 app.use(cors()); // allows input from external URLS 
 const PORT= process.env.PORT || 4000; // 
 
 
 //connecting to mongoDB 
-mangoose.connect(process.env.LINK_URL)
+// mongoose.connect(process.env.LINK_URL)
+mongoose.connect(process.env.LINK_URL)
 .then(()=> app.listen(PORT, ()=> console.log(`server running on port {PORT}`))) // port connected
 .catch((error)=>console.log(error.message)); // port not connected 
 
