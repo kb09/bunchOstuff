bd9ac5b3298f4ce69bdb790ad01da23817c79719
kartik <kabakshi@hotmail.ca>
2022-01-16 20:00:27 -0500

    form state

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import useStyles from './styles';
+
+
+
+
 import {
   TextField, 
   Button,
   Typography,
   Paper
 } from '@material-ui/core';
+import { useState } from 'react';
 
 
 
 const Form = () => {
+  const classes = useStyles();
+
+  const [postData, setPostData] = useState({
+    creator: '', 
+    title: '', 
+    message: '', 
+    tags: '', 
+    selectedFile: ''
+
+
+  });
   return (
+    
 
     <Paper className = {classes.paper}>
 
       <form autoComplete='off' noValidate className={classes.form}>
         <Typography variant='h6'> Create A DejaView to share  </Typography>
-        <TextField name="creator" label="Creator" fullWidth variant="outlined">          
+        <TextField name="creator" 
+        label="Creator" 
+        fullWidth 
+        variant="outlined"
+        value={postData.creator}
+        
+        >          
         </TextField>
 
       </form>
