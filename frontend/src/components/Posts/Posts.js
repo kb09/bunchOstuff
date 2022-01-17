d873d8f23e0f8cd9f6dc1da16d02906f28641681
Victor Bondarenko <nirasimha@gmail.com>
2022-01-17 12:49:46 -0500

    set CurrentId for posts

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Post from './Post/Post';
 import { useSelector } from 'react-redux';
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts)
   console.log(posts);
 
