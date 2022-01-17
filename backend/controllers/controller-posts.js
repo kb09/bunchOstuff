6059bb940e2f104c755b200bd359c8193ec7ae5a
Victor Bondarenko <nirasimha@gmail.com>
2022-01-17 11:44:00 -0500

    update a post if id is valid

--- backend/controllers/controller-posts.js
+++ backend/controllers/controller-posts.js
@@ -34,7 +34,10 @@ export const createPost = async (req, res) => {
 
 export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
+  const post = req.body;
 
   if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id'); //chech if an _id is real db mongoose id
 
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true }); //if id is valid update a post
+  res.json(updatedPost);
 }
