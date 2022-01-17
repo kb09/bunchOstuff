aa2b5a28fd5d3105e6190ab73f7912e28e793eec
Victor Bondarenko <nirasimha@gmail.com>
2022-01-17 11:32:34 -0500

    add route update post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { getPosts, createPost } from "../controllers/controller-posts.js";
+import { getPosts, createPost, updatePost } from "../controllers/controller-posts.js";
 
 const router = express.Router();
 
 // http://localhost:4000/posts
 router.get("/", getPosts); // getPosts is from contorller-posts.js kind of like helper function 
 router.get("/", createPost);
+router.patch('/:id', updatePost); //update post with id
 
 export default router;
