1429aa8e90cf83a5cf887c7fb8a5ba3cc27d828c
Victor Bondarenko <nirasimha@gmail.com>
2022-01-17 17:04:29 -0500

    fix route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // http://localhost:4000/posts
 router.get("/", getPosts); // getPosts is from contorller-posts.js kind of like helper function 
-router.post("/", createPost);
+router.get("/", createPost);
 router.patch('/:id', updatePost); //update post with id
 
 export default router;
