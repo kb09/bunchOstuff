14f3eb7cf27b27be56538eef0ae923f1563009c8
kartik <kabakshi@hotmail.ca>
2022-01-18 15:44:10 -0500

    creating home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,22 @@
-import React, { useEffect, useState} from 'react';
-import { Grid, Container, Grow } from '@material-ui/core';
-import { useDispatch } from 'react-redux'
+import React from 'react';
+import { Container} from '@material-ui/core';
 
-import { getPosts } from "./actions/posts";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from "./styles";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar/Navbar';
-
+import Home from './components/Home/Home';
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts())
-  }, [currentId, dispatch])
-
-
-
-
   
-
-
   return (
-   <Container maxidth="lg">
-     <Navbar/>
-     <Grow in>
-       <Container>
-         <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-          <Grid item xs={12} sm={7}>
-            <Posts setCurrentId={setCurrentId}/>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-          </Grid>
-         </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxidth="lg">
+        <Navbar/>
+        <Switch>
+          
+        </Switch>
+        <Home/>
+      </Container>
+    </BrowserRouter>
   );
 };
 
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Typography, AppBar } from '@material-ui/core';
+import { Typography, AppBar, Button, Toolbar, Avatar } from '@material-ui/core';
 import dejaview from "../../images/dejaview.png";
 import useStyles from "./styles";
+import {Link} from 'react-router-dom';
 
 
 
@@ -10,10 +11,28 @@ const Navbar = () => {
 
   const classes = useStyles();
 
+  const user = null; // fake user
+
   return(
     <AppBar className={classes.appBar} position="static" color="inherit">
-       <Typography className={classes.heading} variant="h2" align="center">DejaView</Typography>
+       <div className={classes.brandContainer}>
+         
+       <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">DejaView</Typography> {/* component={Link} to go to home page */}
        <img className={classes.image} src={dejaview} alt="dejaview" height="60" />
+       
+       </div>
+
+       <Toolbar className={classes.toolbar}>   {/* if user is logged in or if user needs to log in */}
+        {user?.result ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={}>Logout</Button> {/* need to write logout logic still */}
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button> // redirect to auth page
+        )}
+      </Toolbar>
     </AppBar>
   )
 }
