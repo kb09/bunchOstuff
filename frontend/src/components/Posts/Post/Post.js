9deb4877a4c15b89d16b23d49f1a426d395fb098
rom <mozzieisme@gmail.com>
2022-01-18 11:53:57 -0500

    Modify fixes

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -13,10 +13,10 @@ const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
   const dispatch = useDispatch(); //returns a reference to the dispatch
   
-  
   return (
     <Card className={classes.card}>
     <CardMedia className={classes.media} image={post.selectedFile} title = {post.title} />
+      
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
@@ -31,14 +31,19 @@ const Post = ({ post, setCurrentId }) => {
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
+
+
       <Typography className={classes.title} variant="h5" gutterBottom> {post.title} </Typography>
+      
+      
       <CardContent>
-      <Typography variant="h5" gutterBottom> {post.message} </Typography>
+      <Typography variant="body2" color="textSecondary" component="p"> {post.message} </Typography>
       </CardContent>
 
-      <CardActions className={classes.cardActions}> 
 
-      <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
+
+      <CardActions className={classes.cardActions}> 
+      <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> &nbsp; Like &nbsp; {post.likeCount} </Button>
         {/* <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button> */}
       </CardActions>
     </Card>
