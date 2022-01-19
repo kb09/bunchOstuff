1389343d8e058cbb04016efe70951d20ba56ab62
kartik <kabakshi@hotmail.ca>
2022-01-19 00:21:53 -0500

    create signup controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -8,7 +8,7 @@ export const signin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const existingUser = await UserModal.findOne({ email });
+    const existingUser = await UserModal.findOne({ email }); // find user with email
 
     if (!existingUser) return res.status(404).json({ message: "This user does not exist" });
 
@@ -26,3 +26,31 @@ export const signin = async (req, res) => {
 
 
 
+export const signup = async (req, res) => {
+
+
+  const { email, password, firstName, lastName, confirmPassword } = req.body;
+
+  try {
+    const existingUser = await UserModal.findOne({ email }); // check user with email
+
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    if (password !== confirmPassword) return res.status(400).json({ message: "Password does not match" }); 
+
+    const hashedPassword = await bcrypt.hash(password, 12); 
+
+    const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` }); // creates user
+
+    const token = jwt.sign( { email: result.email, id: result._id }, 'test', { expiresIn: "1h" } );
+
+    res.status(200).json({ result, token });
+  } catch (error) {
+
+    res.status(500).json({ message: "There was an erro" });
+    
+    console.log(error);
+
+    
+  }
+};
