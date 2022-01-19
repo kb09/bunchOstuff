9ce657ded9e2261b9e10edb3fd3803aa11b40e85
Victor Bondarenko <nirasimha@gmail.com>
2022-01-18 20:48:59 -0500

    import visibility

--- frontend/src/components/Auth/input.js
+++ frontend/src/components/Auth/input.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
 const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPassword }) => (
   <Grid item xs={12} sm={half ? 6 : 12}>
     <TextField
@@ -16,6 +19,7 @@ const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPas
         endAdornment: (
           <InputAdornment position="end">
             <IconButton onClick={handleShowPassword}>
+              {type === 'password' ? <Visibility /> : <VisibilityOff />}
             </IconButton>
           </InputAdornment>
         ),
