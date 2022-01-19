1d7507271c029487d6fe765753e508e5e53ef8f2
Victor Bondarenko <nirasimha@gmail.com>
2022-01-19 00:07:33 -0500

    add if statement to verify token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,18 @@ const auth = async (req, res, next) => {
   try {
       const token = req.headers.authorization.split(" ")[1];
       const isCustomAuth = token.length < 500; // if lenght is less than 500 then the token is our otherwise external providers
-      
+
+      let decodedData;
+      if (token && isCustomAuth) {      
+        decodedData = jwt.verify(token, secret); // local auth
+  
+        req.userId = decodedData?.id;
+      } else {
+        decodedData = jwt.decode(token); //external auth provider
+  
+        req.userId = decodedData?.sub;
+      }    
+      next(); // all previous statements are good
   } catch (error) {
     console.log(error);
   }
