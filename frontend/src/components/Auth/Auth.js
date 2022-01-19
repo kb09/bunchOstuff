97a1c60a3fa9cfc9bc75bd99cb2ba771fd56ae54
Victor Bondarenko <nirasimha@gmail.com>
2022-01-18 20:03:29 -0500

    import styles and components from material ui

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -1,6 +1,6 @@
 import React from "react";
-
-
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import useStyles from './styles';
 const Auth = () => {
   const state = null;
   return (
