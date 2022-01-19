cd8940564d501ce4392dc1a898c1ce255c2630bd
Victor Bondarenko <nirasimha@gmail.com>
2022-01-19 00:02:10 -0500

    add isCustomAuth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 const auth = async (req, res, next) => {
   try {
       const token = req.headers.authorization.split(" ")[1];
+      const isCustomAuth = token.length < 500; // if lenght is less than 500 then the token is our otherwise external providers
+      
   } catch (error) {
     console.log(error);
   }
