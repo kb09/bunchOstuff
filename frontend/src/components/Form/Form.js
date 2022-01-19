7e3dda14de9e43d05129c178c4f430567cc54898
kartik <kabakshi@hotmail.ca>
2022-01-19 18:26:56 -0500

    fixing styles

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -8,12 +8,10 @@ import FileBase from "react-file-base64";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 
-
 const Form = ({ currentId, setCurrentId }) => {
   const classes = useStyles();
   
   const [postData, setPostData] = useState({
-    creator: '', 
     title: '', 
     message: '', 
     tags: '', 
@@ -21,28 +19,38 @@ const Form = ({ currentId, setCurrentId }) => {
   });
   const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
   const dispatch = useDispatch(); //returns a reference to the dispatch
+  const user = JSON.parse(localStorage.getItem('profile'));
   
   useEffect(() =>  {
     if(post) setPostData(post);
-  }, [post])
+  }, [post]);
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if(currentId) {
+    if(currentId === 0) {
       
-      dispatch(updatePost(currentId, postData));
+      dispatch(createPost({ ...postData, name: user?.result?.name }));
+      clear();
     } else {
       
-      dispatch(createPost(postData));
+      dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
+      clear();
     }
-    clear();
-  }
+  };
   const clear = () => {
    //  add clear functionality 
    setCurrentId(null);
-   setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+   setPostData({ title: '', message: '', tags: '', selectedFile: '' });
+  };
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Sign In to create your own ads and like other's ads.
+        </Typography>
+      </Paper>
+    );
   }
-
      
 
 
@@ -54,19 +62,8 @@ const Form = ({ currentId, setCurrentId }) => {
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">{ currentId ? `Editing "${post.title}"` : 'Post An Ad' }</Typography>
-
+        <Typography variant="h6">{ currentId ? `Editing "${post.title}"` : 'Post Your Ad' }</Typography>
         <TextField
-          name="creator"
-          variant="outlined"
-          label="Creator"
-          fullWidth
-          value={postData.creator}
-          onChange={(element) =>
-            setPostData({ ...postData, creator: element.target.value })
-          }
-        />
-        <TextField 
           name="title"
           variant="outlined"
           label="Title"
@@ -108,27 +105,25 @@ const Form = ({ currentId, setCurrentId }) => {
             }
           />
           <Button
-           className={classes.buttonClear} ///// 
-            variant="contained"
-            color="warning"
-            size="larger"
-            onClick={clear}
-            halfWidth
-          >
-            Clear All
-          </Button>
-
-          <Button
             className={classes.buttonSubmit}
             variant="contained"
             // color="primary"
             size="larger"
             type="submit"
-            halfWidth
+            fullWidth
           >
             Submit
           </Button>
-
+          <Button
+           className={classes.buttonClear} ///// 
+            variant="contained"
+            color="warning"
+            size="medium"
+            onClick={clear}
+            halfWidth
+          >
+            Clear All
+          </Button>
         </div>
       </form>
     </Paper>
