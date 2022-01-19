6ae7cc0680ea37efc38db9b9f0e1d150da39433a
Victor Bondarenko <nirasimha@gmail.com>
2022-01-18 20:44:32 -0500

    add Input function

--- frontend/src/components/Auth/input.js
+++ frontend/src/components/Auth/input.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 
-export default function input() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPassword }) => (
+  <Grid item xs={12} sm={half ? 6 : 12}>
+    <TextField
+      name={name}
+      onChange={handleChange}
+      variant="outlined"
+      required
+      fullWidth
+      label={label}
+      autoFocus={autoFocus}
+      type={type}
+      InputProps={name === 'password' && {
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton onClick={handleShowPassword}>
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  </Grid>
+);
+
+export default Input
