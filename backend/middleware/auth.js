0660ab642a645e622212a483dd01319c39043230
Victor Bondarenko <nirasimha@gmail.com>
2022-01-19 17:35:40 -0500

    replace creator to name

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,12 +2,13 @@ import jwt, {decode} from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
   try {
-      const token = req.headers.Authorization.split(" ")[1];
+      const token = req.headers.authorization.split(" ")[1];
       const isCustomAuth = token.length < 500; // if lenght is less than 500 then the token is our otherwise external providers
 
       let decodedData;
       if (token && isCustomAuth) {
         // fix ::
+        // console.log(req.headers)
         // decodedData = jwt.verify(token, secret); // local auth
         decodedData = jwt.verify(token, "test"); // local auth
   
