83cb9207b06521bb21ebdc1b62c72662da20fcc6
kartik <kabakshi@hotmail.ca>
2022-01-19 00:08:50 -0500

    complete signin controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,4 +3,26 @@ import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
 
-// make sign in and sing up 
+export const signin = async (req, res) => {
+
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await UserModal.findOne({ email });
+
+    if (!existingUser) return res.status(404).json({ message: "This user does not exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Credentials are not valid" });
+
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "There was an error" });
+  }
+};
+
+
+
