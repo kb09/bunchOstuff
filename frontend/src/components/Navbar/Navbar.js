e40ccf95f8e3298f2ed477aa49c7eba4d0fb9684
rom <mozzieisme@gmail.com>
2022-01-19 23:54:11 -0500

    fix few tiny bugs

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = () => {
     if (token) {
       const decodedToken = decode(token)
 
-      if (decodedToken.exp * 1000 < new Date().getTime()) logout()
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
     }
 
     setUser(JSON.parse(localStorage.getItem("profile")));
