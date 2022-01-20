bd2a553c3cf5a2006ed84c9613b6b1417260bdb9
Victor Bondarenko <nirasimha@gmail.com>
2022-01-20 14:27:48 -0500

    add comments

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -55,6 +55,7 @@ const Home = () => {
             <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
+            {/* search bar */}
               <AppBar className={classes.appBarSearch} position="static" color="inherit">
                 <TextField onKeyPress={handleKeyPress} name="search" variant="outlined" label="Search Posts" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
                 <ChipInput
@@ -68,6 +69,7 @@ const Home = () => {
                <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
               </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            {/* pages */}
               <Paper elevation={6}>
                 <Pagination />
               </Paper>
