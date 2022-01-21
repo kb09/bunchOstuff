ccf5bb7ba56cdd17efdb9f614bca5df6daa4ff98
rom <mozzieisme@gmail.com>
2022-01-21 00:07:03 -0500

    Modify the code and implement the pages

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,9 +1,11 @@
 import * as api from "../api";
 
 // Action Creators
-export const getPosts = () => async (dispatch) => {
+export const getPosts = (page) => async (dispatch) => {
   try {
-    const {data} = await api.fetchPosts()
+    const {data} = await api.fetchPosts(page)
+
+    console.log(data);
 
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (err) {
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Post from './Post/Post';
-import {Grid, CircularProgress } from '@material-ui/core';
-import useStyles from './styles';
-
+import React from "react";
+import { useSelector } from "react-redux";
+import Post from "./Post/Post";
+import { Grid, CircularProgress } from "@material-ui/core";
+import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
-
   const classes = useStyles();
 
-  const posts = useSelector((state) => state.posts)
+  const { posts } = useSelector((state) => state.posts);
 
-  return (
-    !posts.length ? <CircularProgress /> : (
-      <Grid className={classes.container} container alignItems="stretch" spacing={3}> {/* 4 per row maybe make 3 */}
-        {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}> 
-
-            <Post post={post} setCurrentId={setCurrentId} /> 
-          </Grid>
-        ))}
-      </Grid>
-    )
+  return !posts?.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {" "}
+      {/* 4 per row maybe make 3 */}
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
