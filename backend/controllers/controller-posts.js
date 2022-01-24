cfd8bdccdb032cbc18ebf279224fa9dad3017d5d
rom <mozzieisme@gmail.com>
2022-01-24 14:34:09 -0500

    Change the limit of cards on your page

--- backend/controllers/controller-posts.js
+++ backend/controllers/controller-posts.js
@@ -10,7 +10,7 @@ export const getPosts = async (req, res) => {
   const { page } = req.query;
 
   try {
-    const LIMIT = 8;
+    const LIMIT = 32;
     const startIndex = (Number(page) - 1) * LIMIT;
     const total = await PostInfo.countDocuments({})
 
